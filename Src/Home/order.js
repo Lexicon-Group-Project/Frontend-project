9246d5ad54b5b657b1cb78a4ae8c21b963ee59f5
Mownikaa <a.mownika@gmail.com>
2022-10-13 17:16:53 +0200

    Adding Order.js,Changes in order.html

---
+++ Src/Home/order.js
@@ -0,0 +1,23 @@
+function attachIngredients() {
+    // var itemSel=  $('#items li');
+        $('#items li').on('click', select);
+     function select() {
+         if($(this).attr('data-selected')){
+             $(this).removeAttr('data-selected');
+             $(this).css('background', "");
+         } else {
+             $(this).attr("data-selected", true);
+             $(this).css('background',"#DDD");
+         }
+     }
+     if($(this).attr("data-selected", true))
+     {
+     $('#showItemsButton').on('click', function () {
+         $('#selectedItems').text("Selected Items: "+$('#items li[data-selected]').toArray().map(li => li.textContent).join(', '));
+     })
+    
+     $('#order').on('click', function () {
+         $('#orderedItems').text("Menu ordered with  Items: "+$('#items li[data-selected]').toArray().map(li => li.textContent).join(', '));
+     })
+     }
+    }
